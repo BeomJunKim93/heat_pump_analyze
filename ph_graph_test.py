da1f2a194a680b90c1c7a3282a2abdf56a3b90b0
BeomJunKim93 <bumji21@hanyang.ac.kr>
2024-01-21 13:23:38 +0900

    Initial commit

---
+++ ph_graph_test.py
@@ -0,0 +1,5 @@
+# Exercise 1 :
+# Two thousand cfm of air at 100 F db and F wb are mixed with 1000 cfm of air at 60 F db and 50 F wb.
+# The process is adiabatic, at a steady flow rate and at standard sea-level pressure.
+# Find the condition of the mixed streams.
+
